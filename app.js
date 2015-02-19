5c415da7bedfc9c71cc1838b7362d8e279208798
Stephen Cattaneo <scattaneo@agari.com>
2015-02-18 22:40:44 -0800

    Adding ability to put draggable object on the floor.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ App = function(){};
 App.prototype.init = function() {
   this.add_control_listeners();
   this.content = $('#content');
+  this._zindex = 10;
 
   $('#controls .update_floor_size').trigger('click');
 };
@@ -13,7 +14,37 @@ App.prototype.add_control_listeners = function() {
 };
 
 App.prototype.add_object = function(event) {
-  // body...
+  $( "#dialog" ).dialog({
+    buttons: [{
+      text: "OK",
+      click: this.generate_object.bind(this)
+    }]
+  });
+};
+
+App.prototype.generate_object = function(event) {
+  // debugger;
+  var button_container = $('<div>');
+  button_container.append($('<button>', {text: '+'}));
+  button_container.append($('<button>', {text: '-'}));
+  button_container.append($('<button>', {text: 'X'}));
+
+  var el = $('<div>', {'class': 'object'});
+  el.append(button_container);
+  el.append($('<div>', {text: $('#dialog .name').val()}));
+
+  el.appendTo('#content');
+  el.css('z-index', this.next_zindex());
+  el.offset($('#content').offset());
+  el.height(this.calc_pixels('#dialog .height'));
+  el.width(this.calc_pixels('#dialog .width'));
+  el.draggable();
+
+  $("#dialog").dialog("close");
+};
+
+App.prototype.next_zindex = function() {
+  return this._zindex++;
 };
 
 App.prototype.calc_pixels = function(selector) {
